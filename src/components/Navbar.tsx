af03e0f0d275e6868f769549ae9109f8d6d98583
Andreyhuey <andreyhuey777@gmail.com>
2023-09-05 22:49:13 +0100

    1st attempt to fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,28 @@
+import { useState, useEffect } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
+import { IoCloseCircleOutline } from "react-icons/io5";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => setNav(!nav);
+
+  // Add this effect to handle body overflow
+  useEffect(() => {
+    if (nav) {
+      document.body.style.overflow = "hidden"; // Prevent scrolling
+    } else {
+      document.body.style.overflow = "auto"; // Allow scrolling
+    }
+    return () => {
+      document.body.style.overflow = "auto"; // Make sure to reset when unmounting
+    };
+  }, [nav]);
+
   return (
-    <div className="text-white px-8 py-4 h-auto fixed top-0 left-0 z-30 bg-[#3A61F0] w-full">
+    <div className="text-white  py-4 h-auto fixed top-0 left-0 z-30 bg-[#3A61F0] w-full">
       {/* Navbar for tabs and pcs*/}
-      <div className="md:flex items-center justify-between relative hidden">
+      <div className="md:flex items-center px-8 justify-between relative hidden">
         <div className="h3">EVO</div>
         <ul
           className="flex items-center justify-between gap-3 absolute top-1/2 left-1/2 
@@ -77,11 +94,46 @@ const Navbar = () => {
       {/*  */}
 
       {/* Navbar for mobile phones */}
-      <div className="md:hidden flex justify-between items-center">
-        <div className="h3">EVO</div>
-        <div className="">
-          <AiOutlineMenu className="w-[40px] h-[40px]" />
+      <div className="md:hidden flex px-8 justify-between items-center">
+        <div className={`h3 ${nav ? "hidden" : ""}`}>EVO</div>
+        <div className="" onClick={handleNav}>
+          {!nav ? (
+            <AiOutlineMenu className="w-[40px] h-[40px]" />
+          ) : (
+            <IoCloseCircleOutline className="text-[26px] fixed right-0" />
+          )}
         </div>
+      </div>
+
+      <div className="w-full">
+        <ul
+          className={
+            !nav
+              ? "hidden"
+              : "absolute h-screen w-full bg-opacity-95 bg-[#3A61F0] transform transition-transform duration-300 ease-in-out"
+          }
+        >
+          <div className="flex flex-col items-center justify-center pt-10 gap-16 w-full">
+            <li onClick={handleNav}>
+              <Link
+                to="home"
+                activeClass="active"
+                smooth={true}
+                spy={true}
+                offset={-200}
+              ></Link>
+            </li>
+            <li>
+              <Link
+                to="home"
+                activeClass="active"
+                smooth={true}
+                spy={true}
+                offset={-200}
+              ></Link>
+            </li>
+          </div>
+        </ul>
       </div>
     </div>
   );
