be42da070442ec2a201a8edd5b2aded1f6108ddf
Andreyhuey <andreyhuey777@gmail.com>
2023-09-06 15:44:11 +0100

    feature Images added to project

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-
 const Services = () => {
   return (
-    <div className="rounded-2xl bg-white min-h-[200vh]" id="services">
-      <></>
+    <div className="rounded-3xl bg-white md:py-24 py-8 mt-20 md:px-20 px-4">
+      <div className="flex flex-col gap-8">
+        <div>
+          <div className="border-[#3A61F0] border-[1px] text-[#3A61F0] border-dashed p-2 font-semibold inline-block rounded-3xl tracking-[3px] uppercase">
+            Features
+          </div>
+        </div>
+        <p className="h2">Features</p>
+        <p className="text-[#333333]">
+          The sky was cloudless and of a deep dark blue.
+        </p>
+      </div>
+
+      <div className="flex flex-col md:flex-row items-center justify-center gap-10">
+        <div className=""></div>
+      </div>
     </div>
   );
 };
