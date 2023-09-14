4bd2fe8ccd057334e440b529d41ce7fd230cd1c5
Andreyhuey <andreyhuey777@gmail.com>
2023-09-14 19:48:51 +0100

    Testimonial data added

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,61 @@
+import jordan from "./assets/test1.svg";
+import cameron from "./assets/test2.svg";
+import harinder from "./assets/test3.svg";
+import jason from "./assets/test4.svg";
+import jose from "./assets/test5.svg";
+import salma from "./assets/test6.svg";
+
+export const TestimonialData = [
+  {
+    id: 0,
+    name: "Jordan Ntolo",
+    story:
+      "A bank is a financial institution licensed to receive deposits and make loans.",
+    displayPicture: jordan,
+    stars: 5,
+  },
+  // {
+  //   id: 1,
+  //   name: "Cameron Rogers",
+  //   story:
+  //     "Banks may also provide financial services such as wealth management, currency exchange, and safe deposit boxes.",
+  //   displayPicture: cameron,
+  //   stars: 5,
+  // },
+  // {
+  //   id: 2,
+  //   name: "Harinder Mondi",
+  //   story:
+  //     "There are several different kinds of banks including retail banks, commercial or corporate banks, and investment banks.",
+  //   displayPicture: harinder,
+  //   stars: 5,
+  // },
+  // {
+  //   id: 3,
+  //   name: "Jason Bailey",
+  //   story:
+  //     "Banking is defined as the business activity of accepting and safeguarding money owned by other individuals.",
+  //   displayPicture: jason,
+  //   stars: 5,
+  // },
+  // {
+  //   id: 4,
+  //   name: "Jose Esteve",
+  //   story:
+  //     "Accepting and safeguarding money owned by other individuals and entities, and then lending out this money in order to conduct economic.",
+  //   displayPicture: jose,
+  //   stars: 5,
+  // },
+  // {
+  //   id: 5,
+  //   name: "Salma Fonseca",
+  //   story:
+  //     "Money in order to conduct economic activities such as making profit or simply covering operating expenses.",
+  //   displayPicture: salma,
+  //   stars: 5,
+  // },
+];
+
 export const faqData = [
   {
     id: 0,
