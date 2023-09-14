5d29d8d2049c137c2f941b76a2644f831854c9e5
Andreyhuey <andreyhuey777@gmail.com>
2023-09-14 19:42:10 +0100

    Testimonial Heading

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,7 +1,20 @@
 const Testimonials = () => {
   return (
-    <div id="stories">
-      <>stories</>
+    <div id="stories" className="px-6 md:px-20 py-10 ">
+      <>
+        <div className="flex flex-col gap-3 items-center justify-center">
+          <div>
+            <div className="border-[#3A61F0] border-[1px] text-[#3A61F0] border-dashed p-2 font-semibold inline-block rounded-3xl tracking-[3px] uppercase hover:scale-110 transition duration-300 ease-in-out  cursor-pointer">
+              Testimonials
+            </div>
+          </div>
+          <div className="text-center h3 lg:w-[425px] leading-[50px]">
+            People Stories
+          </div>
+          <p>Cloudless and of a deep dark blue spectacle before us was.</p>
+        </div>
+      </>
+      <div></div>
     </div>
   );
 };
