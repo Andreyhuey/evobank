5f858fb2b31fa31534722b0bdd02f65d3bbf525b
Andreyhuey <andreyhuey777@gmail.com>
2023-09-15 18:18:03 +0100

    overflow hidden due to inconsistences across devices

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import {
 function App() {
   return (
     <>
-      <div className="bg-[#3A61F0] lg:px-6 md:px-3 flex flex-col gap-6 relative">
+      <div className="bg-[#3A61F0] lg:px-6 md:px-3 flex flex-col gap-6 relative overflow-hidden">
         <Navbar />
         <Header />
         <Services />
