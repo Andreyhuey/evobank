4bcaa479ff8e663e9237bdeed546396cb79d493f
Andreyhuey <andreyhuey777@gmail.com>
2023-09-15 17:03:24 +0100

    About component made responsive for tabs

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,14 +10,14 @@ const About = () => {
   return (
     <div className="md:px-6 py-10" id="about">
       <div className="bg-[#F5F6FB] rounded-3xl md:px-14 px-6 py-14">
-        <div className="flex flex-col md:flex-row justify-between gap-8">
+        <div className="flex flex-col lg:flex-row justify-between gap-8">
           <div className="flex flex-col gap-8">
             <div>
               <div className="border-[#3A61F0] border-[1px] text-[#3A61F0] border-dashed p-2 font-semibold inline-block rounded-3xl tracking-[3px] uppercase hover:scale-110 transition duration-300 ease-in-out  cursor-pointer">
                 About
               </div>
             </div>
-            <p className="h2 lg:max-w-[380px] lg:leading-[60px] font-semibold">
+            <p className="h2 lg:max-w-[380px] md:leading-[60px] font-semibold">
               We Help With Any Problem
             </p>
             <p className="text-[#333333] lg:max-w-[470px]">
