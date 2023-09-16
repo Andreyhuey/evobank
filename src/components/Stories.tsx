59242b4a8fa1dab55eb19feefa849ae059cb3570
Andreyhuey <andreyhuey777@gmail.com>
2023-09-16 15:44:59 +0100

    stories component completed

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,7 +1,50 @@
-const Stories = () => {
+import { AiFillStar } from "react-icons/ai";
+
+interface IconRendererProps {
+  numberOfIcons: number;
+}
+
+const IconRenderer: React.FC<IconRendererProps> = ({ numberOfIcons }) => {
   return (
-    <div>
-      <>Stories</>
+    <div className="flex">
+      {Array.from({ length: numberOfIcons }, (_, index) => (
+        <AiFillStar key={index} className="text-[#F5A647]" />
+      ))}
+    </div>
+  );
+};
+
+interface Prop {
+  index: number;
+  item: {
+    id: number;
+    name: string;
+    displayPicture: string;
+    story: string;
+    stars: number;
+  };
+}
+
+const Stories = ({ item }: Prop) => {
+  const { id, name, displayPicture, story, stars } = item;
+
+  return (
+    <div
+      className="rounded-3xl p-6 flex flex-col gap-4 max-w-[300px] shadow-md"
+      key={id}
+    >
+      <div>
+        <IconRenderer numberOfIcons={stars} />
+      </div>
+      <p>"{story}"</p>
+      <div className="flex gap-3 items-center">
+        <img
+          src={displayPicture}
+          alt={`image of ${name} `}
+          className="h-[40px] w-[40px]"
+        />
+        <p className="h4">{name}</p>
+      </div>
     </div>
   );
 };
