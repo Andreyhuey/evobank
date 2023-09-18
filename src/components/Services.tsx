fc2b32af1c61b7eb167f2a9bd024fb937ffa7497
Andreyhuey <andreyhuey777@gmail.com>
2023-09-18 16:33:12 +0100

    positioning effect for service types corrected

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -13,7 +13,7 @@ const Services = () => {
       {/* Features */}
       <Features />
       {/* Services */}
-      <div className="pt-14 md:px-20 px-4" id="services">
+      <div className="pt-14 md:px-20 px-4 relative" id="services">
         <div className="flex flex-col items-center justify-center gap-8">
           <div>
             <div className="border-[#3A61F0] border-[1px] text-[#3A61F0] border-dashed p-2 font-semibold inline-block rounded-3xl tracking-[3px] uppercase hover:scale-110 transition duration-300 ease-in-out  cursor-pointer">
@@ -21,51 +21,47 @@ const Services = () => {
             </div>
           </div>
           <p className="h2">Explore Services</p>
-          <p className="text-[#333333]">
+          <p className="text-[#333333] pb-[74px]">
             Financial institution licensed to receive deposits and make loans.
           </p>
-          <div className="relative bg-white pb-5">
-            <div className="rounded-2xl inline-block w-full absolute bottom-[-30px]">
-              <div
-                className="flex flex-row items-center justify-center"
-                id="loans"
-              >
-                <a
-                  href="#overview"
-                  className={`${
-                    tabIndex === 1 && "text-[black]"
-                  } text-[#3333337e] border-r px-8 py-2`}
-                  onClick={() => nextTab(1)}
-                >
-                  {" "}
-                  Loans
-                </a>
-                <a
-                  href="#credit-card"
-                  className={`${
-                    tabIndex === 2 && "text-[black]"
-                  } text-[#3333337e] border-r px-8 py-2 text-center`}
-                  onClick={() => nextTab(2)}
-                >
-                  {" "}
-                  Credit Card
-                </a>
-                <a
-                  href="#business"
-                  className={`${
-                    tabIndex === 3 && "text-[black]"
-                  } text-[#3333337e] px-8 py-2`}
-                  onClick={() => nextTab(3)}
-                >
-                  {" "}
-                  Business
-                </a>
-              </div>
-            </div>
+          <div
+            className="flex flex-row items-center justify-center bg-white rounded-3xl shadow-md -bottom-4 absolute"
+            id="loans"
+          >
+            <a
+              href="#loans"
+              className={`${
+                tabIndex === 1 && "text-[black]"
+              } text-[#3333337e] border-r px-8 py-2`}
+              onClick={() => nextTab(1)}
+            >
+              {" "}
+              Loans
+            </a>
+            <a
+              href="#credit-card"
+              className={`${
+                tabIndex === 2 && "text-[black]"
+              } text-[#3333337e] border-r px-8 py-2 text-center`}
+              onClick={() => nextTab(2)}
+            >
+              {" "}
+              Credit Card
+            </a>
+            <a
+              href="#business"
+              className={`${
+                tabIndex === 3 && "text-[black]"
+              } text-[#3333337e] px-8 py-2`}
+              onClick={() => nextTab(3)}
+            >
+              {" "}
+              Business
+            </a>
           </div>
         </div>
       </div>
-      <div className="md:px-6">
+      <div className="md:px-6 ">
         <div className="bg-[#F5F6FB] rounded-3xl px-6 min-h-[50vh] flex flex-col justify-center items-center lg:flex-row py-10 gap-24">
           <img src={Serv} alt="services image" />
           <div className="flex flex-col gap-7 lg:max-w-[400px]">
